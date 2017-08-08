fbe7227c7ba201860f06b56fbb59d919795ea9b1
“amt_masd_th_zhou” <amt_masd_ th_zhou @htc.com>
2017-08-08 18:47:31 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/archermind/coolweather/db/County.java
@@ -0,0 +1,44 @@
+package com.archermind.coolweather.db;
+
+/**
+ * Created by archermind on 17-8-8.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
